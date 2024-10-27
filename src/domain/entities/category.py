3a5fa02997b51d3fe872560afd0d9c6b8704c27e
22Teikk <nguyenkiet228@gmail.com>
2024-10-27 15:07:36 +0700

    Model not need to inherit Entity

--- src/domain/entities/category.py
+++ src/domain/entities/category.py
@@ -1,16 +1,20 @@
 
 from sqlalchemy import Column, ForeignKey, String
 from src.domain.entities.entity import Entity
+from sqlalchemy.orm import relationship
 from src.domain.entities.utils import Base
 
-class Category(Entity):
+class Category(Base):
     __tablename__ = 'category'
-    _id = Column(String(length=36),ForeignKey('entities._id') ,primary_key=True)
+    _id = Column(String(length=36) ,primary_key=True)
     name = Column(String(length=255))
+    # entity = relationship("Entity", backref="category")
+
     def __init__(
         self,
         _id: str,
         name: str):
-        super().__init__(_id)
+        # super().__init__(_id)
+        self._id = _id
         self.name = name
     
