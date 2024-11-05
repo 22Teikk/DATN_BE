07974e2270d4f366fefeb721227b1b1eb0d222e7
22Teikk <nguyenkiet228@gmail.com>
2024-11-05 09:43:13 +0700

    Done Search

--- src/domain/entities/feedback.py
+++ src/domain/entities/feedback.py
@@ -1,4 +1,5 @@
 
+import datetime
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from src.domain.entities.utils import Base
@@ -14,6 +15,17 @@ class Feedback(Base):
     user_id = Column(String(length=36), ForeignKey('User._id'))
     products = relationship("Product", back_populates=__back_populates__)
     users = relationship("UserProfile", back_populates=__back_populates__)
-    def __init__(self, _id: str):
+    def __init__(
+        self, 
+        _id: str,
+        star: int,
+        title: str = None,
+        created_at: datetime.datetime = None,
+        product_id: str = None,
+        user_id: str = None,):
         self._id = _id
-    
+        self.star = star
+        self.title = title
+        self.created_at = created_at if created_at else datetime.datetime.now()
+        self.product_id = product_id
+        self.user_id = user_id
