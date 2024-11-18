2db9a361c0e090e1ea5e6afb603abcf608587c47
22Teikk <nguyenkiet228@gmail.com>
2024-11-18 08:18:06 +0700

    Update Image for PaymentMethod

--- src/domain/entities/payment_method.py
+++ src/domain/entities/payment_method.py
@@ -8,7 +8,9 @@ class PaymentMethod(Base):
     __back_populates__ = 'payment_methods'
     _id = Column(String(length=36) ,primary_key=True)
     name = Column(String(length=255), nullable=False)
+    image_url = Column(String(length=255), nullable=False)
     payments = relationship("Payment", back_populates=__back_populates__)
-    def __init__(self, _id: str, name: str):
+    def __init__(self, _id: str, name: str, image_url: str):
         self._id = _id
         self.name = name
+        self.image_url = image_url
--- tests/container/test_payment_method_container.py
+++ tests/container/test_payment_method_container.py
@@ -1,3 +1,4 @@
+import os
 from src.containers.repository_container import RepositoryContainer
 from src.domain.entities.utils import get_current_timestamp_str, get_new_uuid
 from src.domain.schemas.payment_method_schema import PaymentMethodSchema
@@ -6,14 +7,17 @@ from src.containers.payment_method_container import PaymentMethodContainer
 def test_payment_method_container():
     container = PaymentMethodContainer(RepositoryContainer())
     assert container.usecase is not None
+    host = os.getenv('APP_HOST')
     data = [
         {
             "_id": get_new_uuid(),
             "name": "COD",
+            "image_url": f"{host}/files/bank_card.png"
         },
         {
             "_id": get_new_uuid(),
             "name": "Bank",
+            "image_url": f"{host}/files/cod.jpeg"
         },
     ]
     container.usecase.upserts(data)
