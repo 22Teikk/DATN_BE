efc9bf3abb1f53581cb14c011cf748107b7729ed
22Teikk <nguyenkiet228@gmail.com>
2024-11-19 15:18:42 +0700

    Update Docker compose

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import os
 from src.adapters.database.mysql import MySQL
 from src.frameworks.flask_application import FlaskApplication
 from src.frameworks.socket_io_application import SocketIOApplication
-import logging
-
-# logging.basicConfig(filename='record.log',
-#                 level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 if __name__ == "__main__":
     flask_app = FlaskApplication()
