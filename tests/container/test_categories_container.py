3dc5523d784387318139742134fee8ad4d966d5f
22Teikk <nguyenkiet228@gmail.com>
2024-11-20 21:27:54 +0700

    Update Category

--- tests/container/test_categories_container.py
+++ tests/container/test_categories_container.py
@@ -10,18 +10,22 @@ def test_categories_container():
         {
             "_id": get_new_uuid(),
             "name": "Fast Food",
+            "image_url": "http://94.237.64.46:5001/files/fast_food.png"
         },
         {
             "_id": get_new_uuid(),
-            "name": "WineAlcoholic beverages",
+            "name": "Cake",
+            "image_url": "http://94.237.64.46:5001/files/cake.png"
         },
         {
             "_id": get_new_uuid(),
-            "name": "Juice",
+            "name": "Dessert",
+            "image_url": "http://94.237.64.46:5001/files/dessert.png"
         },
         {
             "_id": get_new_uuid(),
             "name": "Coffee",
+            "image_url": "http://94.237.64.46:5001/files/coffee.png"
         }
     ]
     container.usecase.upserts(data)
